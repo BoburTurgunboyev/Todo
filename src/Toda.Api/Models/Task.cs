7b12c8525f5d8ba251338ec02d3b4cf64d432cca
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-27 14:42:36 +0500

    model created

---
+++ src/Toda.Api/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using Toda.Api.Enums;
+
+namespace Toda.Api.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Priority { get; set; }
+        public State State { get; set; }
+
+    }
+}
+
+
+
