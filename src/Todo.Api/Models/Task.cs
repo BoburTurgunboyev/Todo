b90d1375d0e0221e7127849b7446227241e589a5
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-27 14:51:25 +0500

    recommit type error in created file

--- src/Todo.Api/Models/Task.cs
+++ src/Todo.Api/Models/Task.cs
@@ -7,8 +7,8 @@ namespace Toda.Api.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime DueDate { get; set; }
         public string Priority { get; set; }
+        public DateTime DueDate { get; set; }
         public State State { get; set; }
 
     }
