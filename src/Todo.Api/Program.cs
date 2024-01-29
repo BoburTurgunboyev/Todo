f9e182a2876b93778feb71ac980bf4f1a06771db
BoburTurgunboyev <turgunboyevbbobur7881@mail.ru>
2024-01-29 13:23:34 +0500

    have change in program.cs

--- src/Todo.Api/Program.cs
+++ src/Todo.Api/Program.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.Api.Data;
+using Todo.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ITaskentityService,TaskentityService>();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer
 (builder.Configuration.GetConnectionString("Connect")));
